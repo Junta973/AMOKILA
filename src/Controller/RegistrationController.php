5a12da4ab12d3a8db9f0af89ccc8c7fbb57c29e7
chahir jbali <chahirjbali@gmail.com>
2021-08-14 23:45:15 +0100

    Design page admin.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -17,28 +17,35 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $passwordEncoder->encodePassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+        if ($form->isSubmitted()) {
+            if ($form->isValid()){
+                // encode the plain password
+                $user->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
 
 
-            $user->setUsername('marc');
-            //dd($user);
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
-
-            return $this->redirectToRoute('app_login');
+                $user->setUsername('marc');
+                //dd($user);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+                // do anything else you need here, like send an email
+                $this->addFlash('danger','Inscription effectué avec succès!');
+                return $this->redirectToRoute('app_login');
+            }else{
+                $this->addFlash('danger','veuillez vérifier le formulaire!');
+            }
         }
 
         return $this->render('registration/register.html.twig', [
